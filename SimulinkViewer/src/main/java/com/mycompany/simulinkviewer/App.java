a15daaf12b20b3fb9d3cea944f2672bebc15a64e
Mohammed Yasser <mohdyasser100@gmail.com>
2023-05-15 16:35:02 +0300

    Rectangle Test
    Testing how the Rectangles should be added in the GUI

--- SimulinkViewer/src/main/java/com/mycompany/simulinkviewer/App.java
+++ SimulinkViewer/src/main/java/com/mycompany/simulinkviewer/App.java
@@ -1,22 +1,65 @@
 package com.mycompany.simulinkviewer;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.*;
+import javafx.scene.layout.*; 
+import javafx.scene.shape.*;
 
 
-/**
- * JavaFX App
- */
+
+/*
+Ahmed/Omar TODO:
+class Block:
+    static int Num:number of blocks.
+    static int getNum(): a static method which returns the number of blocks.
+    int getSID()
+    //Getting the position of the block
+    //If you have a better idea go ahead
+    int getLeft()
+    int getTop()
+    int getRight()
+    int getBottom()
+
+    int getMinLeft(): Gets the minimum left position of a block to place it first and to be a refrence
+    
+*/
+class Block{
+    private static int Num;
+    
+    public static int getNum(){
+        return Num;
+    }
+}
 public class App extends Application {
 
     @Override
     public void start(Stage stage) {
         //Create a Stack pane because I think it is the best option
         //-->But we have to make sure that the blocks are not overlapping.
-        StackPane pane = new StackPane();
+        Pane pane = new Pane();
+
+
+        // Block Add
+        Rectangle blockAdd = new Rectangle(1040, 209, 30, 32);
+        pane.getChildren().add(blockAdd);
+
+        // Block Constant
+        Rectangle blockConstant = new Rectangle(780, 200, 30, 30);
+        pane.getChildren().add(blockConstant);
+
+        // Block Saturation
+        Rectangle blockSaturation = new Rectangle(935, 200, 30, 30);
+        pane.getChildren().add(blockSaturation);
+
+        // Block Scope
+        Rectangle blockScope = new Rectangle(1130, 209, 30, 32);
+        pane.getChildren().add(blockScope);
+
+        // Block Unit Delay
+        Rectangle blockUnitDelay = new Rectangle(1040, 283, 35, 34);
+        pane.getChildren().add(blockUnitDelay);
+        
         Scene scene = new Scene(pane, 640, 480);//What are the window size we want ? --> 640, 480
         /*
         According to the mdl file there is position wish indicates the position on the screen
